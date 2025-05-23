80b0030ac3f9c7ef4eb55fe84dc033cf28045ba6
Ja Tar <tarnogorskijan@gmail.com>
2025-05-23 22:02:48 +0200

    options page

---
+++ options/options.js
@@ -0,0 +1,13 @@
+
+// REMOVE: When ready
+// document.getElementById('personal-access-token').addEventListener('change', function () {
+//     document.getElementById('token').classList.toggle('disabled', !this.checked);
+//     document.getElementById('token').toggleAttribute('disabled', !this.checked);
+//     document.getElementById('login-button').classList.toggle('disabled', this.checked);
+// });
+// document.getElementById('oauth').addEventListener('change', function () {
+//     document.getElementById('token').classList.toggle('disabled', this.checked);
+//     document.getElementById('token').toggleAttribute('disabled', this.checked);
+//     document.getElementById('login-button').classList.toggle('disabled', !this.checked);
+// });
+
