84d6db198194e61751e32d6643588ca6348e4d0b
Ja Tar <tarnogorskijan@gmail.com>
2025-06-04 15:50:29 +0200

    final fix 7

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 // Background script
 
-browser.runtime.onInstalled.addListener(function() {
+browser.runtime.onInstalled.addListener(() => {
     console.log("TickBack extension installed");
     browser.storage.local.get('token').then((result) => {
         if (!result.token) {
